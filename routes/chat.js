91027902498dc11e17fb26e18d194d0c036a9004
ashirwad0201 <ashirwad0201@gmail.com>
2024-03-02 20:23:30 +0530

    bug fix

--- routes/chat.js
+++ routes/chat.js
@@ -3,13 +3,23 @@ const fs=require('fs');
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<form action="/" method="POST"><input type="text" name="chat"><button type="submit">Send</button></form>');
+    const filepath='./messages.txt';
+    fs.readFile(filepath,'utf8',(err,data)=>{
+        if(!err){
+            console.log('In another middleware');
+            res.send(`<form action="/" method="POST"><p><pre>${data.toString()}</pre></p><input type="text" name="chat"><input id="idk2" type="hidden" name="username"><button type="submit">Send</button></form><script>document.getElementById('idk2').value=localStorage.getItem('username');
+            </script>`);        
+        }
+        else{
+            alert('Something went wrong')
+        }
+    })
 });
 router.post('/',(req,res,next)=>{
     const message = req.body.chat;
+    const user=req.body.username;
 
-    fs.appendFileSync('messages.txt', `${message}\n`);
+    fs.appendFileSync('messages.txt', `${user + ' : ' + message}\n`);
     console.log(req.body);
     res.redirect('/');
     });
